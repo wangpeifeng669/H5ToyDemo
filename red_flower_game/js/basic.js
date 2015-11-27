56b0c6def3eeec5a1a87a3df1bf3c4ccf8b7e6ba
peter_wang <597927969@qq.com>
2015-11-27 08:44:51 +0800

    red flower game demo:add top view and center tree view

---
+++ red_flower_game/js/basic.js
@@ -0,0 +1,22 @@
+﻿/*初始化完页面自动调用*/
+$(function () {
+    // setup garden
+    refreshItemHeight();
+});
+
+/*页面尺寸发生变化促发*/
+$(window).resize(function() {
+	refreshItemHeight();
+});
+
+function refreshItemHeight() {
+	var newWidth = $(window).width();
+	var newHeight = $(window).height();
+
+	//设置顶部小红花高度和水平居中显示
+	$("#flower-game-icon").css("height", $("#flower-game-icon").width()*32/45);
+	$("#flower-game-icon").css("margin-left", (newWidth-$("#flower-game-icon").width())/2);
+
+    //设置顶部小红花高度和水平居中显示
+	$("#tree-part").css("height", $("#tree-part").width()*91/84);
+}
