c94df08ab709da5e73c67a579f49489db952b22e
peter_wang <597927969@qq.com>
2015-11-28 10:22:13 +0800

    red flower game demo:add flower animation and point count

--- red_flower_game/js/basic.js
+++ red_flower_game/js/basic.js
@@ -1,14 +1,46 @@
-﻿/*初始化完页面自动调用*/
+﻿//显示的花数
+var flowerNum = 0;
+//需要完成的目标分数
+var point = 5;
+//分数
+var finishPoint = 0;
+
+
+/*初始化完页面自动调用*/
 $(function () {
     // 初始化 UI 尺寸
     refreshItemHeight();
 
     $("#bottom-part #add-flower").click(function(){
-    	$("#tree-part").prepend('<span id="flower" class="icon icon-flower pos-abs"></span>');
-    	$("#tree-part "+"#flower").css({"top":"100%","left":"50%"});
-    	var randomTop = 20+Math.random()*50+"%";
+    	flowerNum++;
+    	var randomFlowerType = Math.random();
+    	if (randomFlowerType<0.6) {
+    		$("#tree-part").prepend('<span id="'+flowerNum+'" class="icon icon-flower-color pos-abs"></span>');
+    		finishPoint=finishPoint+1;
+    	} 
+    	else if(randomFlowerType<0.8) {
+    		$("#tree-part").prepend('<span id="'+flowerNum+'" class="icon icon-flower pos-abs"></span>');
+    		finishPoint=finishPoint+3;
+    	} 
+    	else if(randomFlowerType<0.95){
+    		$("#tree-part").prepend('<span id="'+flowerNum+'" class="icon icon-sports pos-abs"></span>');
+    		finishPoint=finishPoint+5;
+    	}
+    	else{
+    		$("#add-flower").addClass("hidden");
+    		$(".flame-part-ground").removeClass("hidden");
+    		$(".flame").css({"opacity":"1","transition":"0.4s linear"});
+    		$(".lid").css({"animation":"lidOff 0.3s linear","animation-fill-mode":"forwards"});
+
+    		setTimeout("fireAllFlowers()",3000);
+    	}
+    	$("#finished-point").text(finishPoint);
+    	$("#tree-part #"+flowerNum).css({"top":"100%","left":"50%"});
+    	//距离顶部范围10%~70%
+    	var randomTop = 10+Math.random()*60+"%";
+    	//距离左部范围10%~90%
     	var randomLeft = 10+Math.random()*80+"%";
-    	$("#tree-part "+"#flower").animate({
+    	$("#tree-part #"+flowerNum).animate({
     		top:randomTop,
     		left:randomLeft,
     		opacity:'0.5',
@@ -31,4 +63,15 @@ function refreshItemHeight() {
 
     //设置顶部小红花高度和水平居中显示
     $("#tree-part").css("height", $("#tree-part").width()*91/84);
+}
+
+/*燃烧花朵，清除花朵和分数*/
+function fireAllFlowers() {
+	$(".flame-part-ground").addClass("hidden");
+	$("#add-flower").removeClass("hidden");
+    //清除所有花朵
+    $("#tree-part span").remove(".icon");
+    //清除分数
+    finishPoint=0;
+    $("#finished-point").text(finishPoint);
 }
